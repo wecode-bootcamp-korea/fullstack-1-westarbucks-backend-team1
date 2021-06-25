97bd1a8ff747843254dccc9990e7bdf810fa1dbf
hxyxneee <wlgus3096@gmail.com>
2021-06-25 15:35:21 +0900

    MODIFY: signup 파일 - 중복 정보 입력 시 발생하는 에러 함수 추가

--- jihyunkim-api/signup.js
+++ jihyunkim-api/signup.js
@@ -1,9 +1,17 @@
+/* eslint-disable no-unused-vars */
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 const userSignUp = async (req, res) => {
   try {
     const { email, name, password } = req.body;
+
+    const user = await prisma.$queryRaw(`
+      SELECT * FROM users WHERE email='${email}';
+    `);
+    if (user.length) {
+      res.status(400).json({ message: '이미 존재하는 유저의 정보입니다!' });
+    }
 
     const createdUser = await prisma.$queryRaw(`
         INSERT INTO users(email, name, password) 
@@ -14,7 +22,7 @@ const userSignUp = async (req, res) => {
       message: '사용자가 생성한 정보입니다.',
     });
   } catch (err) {
-    res.status(500).json({ message: '에러 발생! 에러 발생! ☄️' });
+    res.status(500).json({ message: err.message });
   }
 };
 
