f56213a0f007edffcf36c2a69b5f4caa6b753905
hxyxneee <wlgus3096@gmail.com>
2021-06-25 16:50:40 +0900

    ADD: /drinks 엔드 포인트 생성

--- jihyunkim-api/Router/router.js
+++ jihyunkim-api/Router/router.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import userSignUp from '../signup';
 import allUsers from '../users';
+import drinksInfo from '../drinks';
 const router = express.Router();
 
 router.post('/users/signup', userSignUp);
 router.get('/users', allUsers);
+router.get('/drinks', drinksInfo);
 
 export default router;
