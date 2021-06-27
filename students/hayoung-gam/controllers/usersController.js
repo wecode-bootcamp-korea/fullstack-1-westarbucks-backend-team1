63d9a8e7920d35e1feb66ea655cd35d9f74258b2
Hayoung Gam <hahhyoung@gmail.com>
2021-06-27 21:06:51 +0900

    Refactor: exists문을 사용하여 email 데이터가 없을때만 user 추가

--- students/hayoung-gam/controllers/usersController.js
+++ students/hayoung-gam/controllers/usersController.js
@@ -16,19 +16,9 @@ export const signUp = async (req, res) => {
   try {
     const { email, name, password } = req.body;
 
-    const user = await prisma.$queryRaw(`
-      SELECT * FROM users WHERE email='${email}';
+    const createdNewUser = await prisma.$queryRaw(`
+      INSERT INTO users (email, name, password) SELECT '${email}', '${name}', '${password}' WHERE NOT EXISTS (SELECT email FROM users WHERE email='${email}');
     `);
-
-    if (user.length) {
-      res
-        .status(400)
-        .json({ message: 'THIS_EMAIL_ADDRESS_IS_ALREADY_BEING_USED' });
-    }
-
-    const createdUser = await prisma.$queryRaw(
-      `INSERT INTO users(email, name, password) VALUES ('${email}', '${name}', '${password}');`
-    );
 
     res.status(201).json({ name, email, password });
   } catch (err) {
