6ad2456d57f6badc4d5c1c8bcc9f8b8a2eed7cc2
hxyxneee <wlgus3096@gmail.com>
2021-06-28 16:39:23 +0900

    ADD: hashing password 구현 완료

--- jihyunkim-api/models/signupDao.js
+++ jihyunkim-api/models/signupDao.js
@@ -1,8 +1,13 @@
 /* eslint-disable no-unused-vars */
 import prisma from '../prisma';
+import bcrypt from 'bcrypt';
 
 const userSignUp = async (req) => {
   const { email, name, password } = req.body;
+  const saltRounds = 10;
+
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hashedPassword = bcrypt.hashSync(password, salt);
 
   const createdUser = await prisma.$queryRaw(`
         INSERT INTO users(email, name, password) 
