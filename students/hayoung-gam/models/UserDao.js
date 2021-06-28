c2458275401529550d10395386a57e6393c8cf49
Hayoung Gam <hahhyoung@gmail.com>
2021-06-28 12:51:58 +0900

    Add: 회원가입 비밀번호 hashing 구현

--- students/hayoung-gam/models/UserDao.js
+++ students/hayoung-gam/models/UserDao.js
@@ -1,4 +1,5 @@
 import prisma from '../prisma';
+import bcrypt from 'bcrypt';
 
 const getUsersList = async () => {
   const users = await prisma.$queryRaw(`SELECT * FROM users;`);
@@ -8,10 +9,14 @@ const getUsersList = async () => {
 
 const postSignUp = async (req) => {
   const { email, name, password } = req.body;
+  const saltRounds = 10;
+
+  const salt = await bcrypt.genSalt(saltRounds);
+  const hashedPassword = await bcrypt.hash(password, salt);
 
   const signUp = await prisma.$queryRaw(`
-      INSERT INTO users (email, name, password) SELECT '${email}', '${name}', '${password}' WHERE NOT EXISTS (SELECT email FROM users WHERE email='${email}');
-    `);
+    INSERT INTO users (email, name, password) SELECT '${email}', '${name}', '${hashedPassword}' WHERE NOT EXISTS (SELECT email FROM users WHERE email='${email}');
+  `);
 
   return signUp;
 };
