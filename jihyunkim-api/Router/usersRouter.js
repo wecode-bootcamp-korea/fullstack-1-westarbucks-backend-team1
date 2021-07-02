0d213a1c995d91e2c98162a7d6e80773283f83c4
hxyxneee <wlgus3096@gmail.com>
2021-07-02 17:41:08 +0900

    ADD: users - 토큰 authentication 추가

--- jihyunkim-api/Router/usersRouter.js
+++ jihyunkim-api/Router/usersRouter.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { usersController } from '../controllers';
+import authenticateToken from '../middleware/verify_jwt';
 
 const router = express.Router();
 
 router.get('', usersController.findAllUsers);
 router.post('/signup', usersController.userSignUp);
 router.post('/login', usersController.userLogin);
-router.put('', usersController.changePassword);
-router.put('/update', usersController.changeEmail);
+router.put('', authenticateToken, usersController.changePassword);
+router.put('/update', authenticateToken, usersController.changeEmail);
 
 export default router;
